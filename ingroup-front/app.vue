aa617b7cc5066b06095bdb5e66ee126bbfb9dc90
Tom Scanlan <tompscanlan@gmail.com>
2024-08-07 19:05:22 -0400

    test

--- ingroup-front/app.vue
+++ ingroup-front/app.vue
@@ -1,6 +1,30 @@
+<script setup lang="ts">
+const version = 2 + 1
+
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+const sameCounter = useState('counter')
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="hello">
+    Hello Nuxt {{ version }}!
   </div>
+
+    <NuxtExample dir="features/state-management">
+    <p>Counter: {{ counter }}</p>
+    <div class="flex gap-2 my-4">
+      <UButton @click="counter--">-</UButton>
+      <UButton @click="counter++">+</UButton>
+    </div>
+    <p>Same Counter: {{ sameCounter }}</p>
+  </NuxtExample>
 </template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
